b1243fb9fdb204775446d9d7cbd0e9e1651b387e
Leon <leon.andreasen@skola.botkyrka.se>
2020-12-07 15:39:43 +0100

    function complete but not working

--- sorting.js
+++ sorting.js
@@ -1,10 +1,15 @@
 import { randomArray, swap } from './helpers.js'
 
 const a = randomArray(100, 10)
+
+const arr = [5,8,3,1]
+function bubblesort(a){
 for (let j = 0; j < a.length -1;  j++) {
     for (let i = 0; i < a.length -j -1; i++) {
     if (a[i]>a[i+1]) {
     swap(a,i ,i, + 1)  
     }
-    
-}}
+    }}
+}
+bubblesort(arr)
+console.log(arr)
