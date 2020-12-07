0c4ebebe05feb5dcbfddd768700d384ee54e1e7c
Leon <leon.andreasen@skola.botkyrka.se>
2020-12-07 15:01:57 +0100

    function complete

--- sorting.js
+++ sorting.js
@@ -1,10 +1,10 @@
 import { randomArray, swap } from './helpers.js'
 
 const a = randomArray(100, 10)
-for (let j = 0; j < a.length; j++) {
-    for (let i = 0; i < a.length; i++) {
+for (let j = 0; j < a.length -1;  j++) {
+    for (let i = 0; i < a.length -j -1; i++) {
     if (a[i]>a[i+1]) {
-    swap()  
+    swap(a,i ,i, + 1)  
     }
     
 }}
