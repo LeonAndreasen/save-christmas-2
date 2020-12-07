630c8eeb49936b4f57565e8303555ca91d240607
Leon <leon.andreasen@skola.botkyrka.se>
2020-12-07 14:56:21 +0100

    structure

--- sorting.js
+++ sorting.js
@@ -1,9 +1,10 @@
 import { randomArray, swap } from './helpers.js'
 
 const a = randomArray(100, 10)
-
-for (let i = 0; i < a.length; i++) {
-    const element = array[i];
+for (let j = 0; j < a.length; j++) {
+    for (let i = 0; i < a.length; i++) {
+    if (a[i]>a[i+1]) {
+    swap()  
+    }
     
-    swap()
-}
+}}
